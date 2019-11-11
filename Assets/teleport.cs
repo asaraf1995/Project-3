65b218511237a1a5260600a5862d5b6698f4379d
Katherine Misyutina <kmisyu2@uic.edu>
2019-11-10 19:05:01 -0600

    basic rooms plus teleporting

---
+++ Assets/teleport.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport : MonoBehaviour
+{
+    public GameObject obj;
+    public GameObject button;
+    public GameObject wall;
+    public GameObject teleportPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("wall")) {
+            obj.transform.position = new Vector3(teleportPoint.transform.position.x, obj.transform.position.y, 
+                teleportPoint.transform.position.z);
+        }
+    }
+
+}
+
