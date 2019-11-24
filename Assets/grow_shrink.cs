c9a1509504d15c7f6088c49f9d7c8cb38fedc005
Katherine Misyutina <kmisyu2@uic.edu>
2019-11-23 18:33:23 -0600

    Revert "Revert "added all statues and letter wheels.""
    This reverts commit f176b7b7571953b9fa1c82eaac59b2e7a36dc5b3.

---
+++ Assets/grow_shrink.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grow_shrink : MonoBehaviour
+{
+	public GameObject alias;
+	public float x_change = 9.6f;
+	public float y_change = 4.0f;
+	public float z_change = -35.4f;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollision(Collision other)
+    {
+        alias.transform.localPosition = new Vector3(x_change, y_change, z_change);
+
+    }
+
+}
