e86a420f4fa8081c03ccd5f0370e182cb8a9a2b4
asaraf1995 <aasthasaraf1995@gmail.com>
2019-11-24 14:54:24 -0600

    new models, scripts, puzzle started

---
+++ Assets/wand_collision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wand_collision : MonoBehaviour
+{
+    public GameObject box;
+    public GameObject trophy;
+    
+    public void OnCollisionEnter(Collision collision)
+    {
+
+            box.SetActive(false);
+            trophy.SetActive(true);
+  
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
